8df6f6fd3096b0fa5e1ba23bec724e4918dc5ea0
zhangfuli <1171067930@qq.com>
2017-04-26 22:37:18 +0800

    两次实验报告

--- dataStructure/排序算法的应用.cpp
+++ dataStructure/排序算法的应用.cpp
@@ -1,7 +1,6 @@
 #include "assert.h"
 #include <iostream>
 using namespace std;
-
 void excg(int *a,int *b){
     int *temp =  new int;
     *temp = *a;
@@ -13,8 +12,7 @@ void Sort(int a[],int n){
     start = &a[0];
     last = &a[n-1];
     active = start  ;
-
-        while(active<=last){
+    while(active<=last){
             if(*active==2){
                 active = active+1;
 
@@ -23,36 +21,22 @@ void Sort(int a[],int n){
                         cout<<active-&a[0]<<"<--->"<<last-&a[0]<<endl;
                     }
                     excg(active,last);
-             //   active = active + 1;
                     last = last - 1;
-
-
-
             }else{
                 if(active-&a[0]!=start-&a[0]){
-                        cout<<start-&a[0]<<"<--->"<<active-&a[0]<<endl;
-                    }
+                    cout<<start-&a[0]<<"<--->"<<active-&a[0]<<endl;
+                }
 
-                    excg(active,start);
-                    active = active + 1;
-                    start = start + 1;
-
+               	excg(active,start);
+                active = active + 1;
+                start = start + 1;
             }
         }
-
-
     for(int i=0;i<n;i++){
         cout<<a[i];
     }
     cout<<endl;
 }
-
-
-
-
-
-
-
 int main() {
 	int n;
 	cin >> n;
